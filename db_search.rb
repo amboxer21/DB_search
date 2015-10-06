a53abd3d194bcfbba29da6f600ce1c4651f2d791
Anthoyn Guevara <amboxer21@gmail.com>
2015-10-06 15:50:09 -0400

    .

--- db_search.rb
+++ db_search.rb
@@ -2,13 +2,13 @@
 
 require 'mysql2'
 
+$show_tables,$dataBases,$tables,$explain = [],[],[],[]
+
 def cleanup(var)
   return var.to_s.gsub(/\[|\]|\"/,"")
 end
 
-$show_tables,$dataBases,$tables,$explain = [],[],[],[]
-
-def connect(db,command,action)
+def connect(db,command,action,table)
   dbf = cleanup(db)
   client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "easypeasy", :database => dbf)
   results = client.query(command)
@@ -18,21 +18,27 @@ def connect(db,command,action)
     elsif action == "query_tables"
       $tables.push db1.values
     elsif action == "explain_tables" 
-      $explain.push db1.values[0]
-    elsif action == "traverse_db"
-      puts db1  
-      puts "TEST"
-    elsif action.nil?
-      puts "TEST"
-      client.close
+      $explain.push db1.values
+
+      $explain.each   do |explain|
+        x = cleanup(explain)
+        if x =~ /#{ARGV[0]}/
+          puts "FOUND \"#{ARGV[0]}\" in => DATABASES(#{dbf}), TABLE(#{table})"
+          exit
+        else
+          puts "X - #{x}"
+        end
+      end
+
+      #sleep 1
     end
   end
 end
 
-connect("hpbx_development","show databases","initial_query")
+connect("hpbx_development","show databases","initial_query",nil)
 
 $dataBases.each do |dbs|
-  connect(dbs,"show tables","query_tables")
+  connect(dbs,"show tables","query_tables",nil)
   $tables.each do |tables|
     $show_tables.push "#{dbs},#{tables}"
   end
@@ -41,25 +47,9 @@ end
 $show_tables.each do |tables|
   begin
     hash_split = cleanup(tables).split(/,/)
-    connect(hash_split[0], "explain #{hash_split[1]}","explain_tables")
+    connect(hash_split[0], "select * from #{hash_split[1]}","explain_tables",hash_split[1])
+    #sleep 1
   rescue
     next
   end
-end
-
-$dataBases.each do |dbs|
-  $tables.each do |tables|
-    $explain.each do |finalOne|
-        #puts "#{cleanup(dbs)},#{cleanup(tables)},#{cleanup(finalOne)}"
-      begin
-        # Does not run with this below disconnect. I assume its due to me closing the connection after every pass. So it reiterates over the arrays from the beginning.
-        #connect(cleanup(dbs),"select * from #{cleanup(tables)} where #{cleanup(finalOne)} like \"%#{ARGV[0]}%\"","traverse_db") unless finalOne.empty?
-        sleep 1
-        puts "#{cleanup(dbs)}, \"select * from #{cleanup(tables)} where #{cleanup(finalOne)} like \"%#{ARGV[0]}%\"\",\"traverse_db\"" unless finalOne.empty?
-        connect(cleanup(dbs),"select * from #{cleanup(tables)} where #{cleanup(finalOne)} like \"%#{ARGV[0]}%\"",nil) unless finalOne.empty?
-      rescue
-        next
-      end
-    end
-  end  
 end
